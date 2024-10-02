41fa92c23714d4edd8708b15996c9205a05447db
goblinious1 <striped_dibble_0j@icloud.com>
2024-10-02 17:35:00 -0500

    Set Touch -> Mouse emulation

--- High School Helper.service.worker.js
+++ High School Helper.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1727907545|5280794298';
+const CACHE_VERSION = '1727908437|6172360034';
 /** @type {string} */
 const CACHE_PREFIX = 'Fast2Class-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
