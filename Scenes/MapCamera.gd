b589bee85e9797d32c333d9a128e1b6c06da8f5d
goblinious1 <striped_dibble_0j@icloud.com>
2024-10-04 18:44:24 -0500

    lower sens

--- Scenes/MapCamera.gd
+++ Scenes/MapCamera.gd
@@ -19,7 +19,7 @@ func _input(event):
 		%Console.text = str("tip tap!") + "\n" + %Console.text
 	if event is InputEventMultiScreenDrag and event.fingers == 3:
 		%Console.text = str("MultiDrag!") + "\n" + %Console.text
-		get_parent().rotation.x += event.relative.y / 600
+		get_parent().rotation.x += event.relative.y / 16000
 	if event is InputEventScreenPinch and event.fingers == 2:
-		position = position.move_toward(Vector3.ZERO, event.relative / 100)
+		position = position.move_toward(Vector3.ZERO, event.relative / 900)
 		
