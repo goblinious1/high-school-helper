88cbc49629d9795d88cf9506f45a88b40047173d
goblinious1 <striped_dibble_0j@icloud.com>
2024-10-04 18:02:50 -0500

    Completely Redo Gestures with GTIM

--- Export/High School Helper.service.worker.js
+++ Export/High School Helper.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1727908437|6172360034';
+const CACHE_VERSION = '1728082888|161178512544';
 /** @type {string} */
 const CACHE_PREFIX = 'Fast2Class-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
--- Scenes/MapCamera.gd
+++ Scenes/MapCamera.gd
@@ -1,24 +1,25 @@
 extends Camera3D
-
-var lastEvPos = null
-
+var lastEvPos
 func _on_static_body_3d_input_event(camera, event, event_position, normal, shape_idx):
-	#if event is InputEventMouseButton:
-	#	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-	#		return
-	#	position = Vector3(event_position.x, position.y, event_position.z) 
 	if event is InputEventScreenTouch:
 		lastEvPos = event_position
+		return
 	if event is InputEventScreenDrag and lastEvPos != null:
 		var move:Vector3 = (lastEvPos - event_position)
+		
 		
 		if move.length() > 180:
 			print("Canceled drag because distance was too great")
 			lastEvPos = null
 			return
 		get_parent().position += move
-	if event is InputEventMagnifyGesture:
-		position = position.move_toward(get_parent().position, event.delta)
-	if event is InputEventPanGesture:
-		get_parent().rotation.x += event.delta.y
+
+func _input(event):
+	if event is InputEventSingleScreenLongPress:
+		%Console.text = str("tip tap!") + "\n" + %Console.text
+	if event is InputEventMultiScreenDrag and event.fingers == 3:
+		%Console.text = str("MultiDrag!") + "\n" + %Console.text
+		get_parent().rotation.x += event.relative.y / 2
+	if event is InputEventScreenPinch and event.fingers == 2:
+		position = position.move_toward(get_parent().position, event.relative / 10)
 		
