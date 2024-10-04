d312a49832703e34f50fba44c13a222947a150ec
goblinious1 <striped_dibble_0j@icloud.com>
2024-10-04 18:44:24 -0500

    lower sens

--- High School Helper.service.worker.js
+++ High School Helper.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1728083503|161793317202';
+const CACHE_VERSION = '1728085442|163732885489';
 /** @type {string} */
 const CACHE_PREFIX = 'Fast2Class-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
